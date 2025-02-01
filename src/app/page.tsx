df8897e2bd439b286a4cf9d7a656e3fd36598ec2
Fabian Zambrano <zambranosalcedo@gmail.com>
2025-02-01 15:11:31 +0100

    align-div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,16 @@ import styles from "./page.module.css";
 import InputText from "~/ui/InputText";
 import BtnAdd from "~/ui/BtnAdd";
 
-
 export default function Home() {
   return (
-    <div>
+    <div className="todo-list">
       <h1>To Do List</h1>
-      <div>
+      <div className="todo-list-inputText">
         <InputText onChange={(text) => {
           console.log(text);
         }}/>
       </div>
-      <div>
+      <div className="todo-list-btnAdd">
         <BtnAdd className='container'>Enviar</BtnAdd>
       </div>
     </div>
